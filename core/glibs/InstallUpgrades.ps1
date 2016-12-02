15c1b63c14e01a10f88c03abe68d999dae755299
rrg92 <rodrigor.gomes@hotmail.com>
2016-12-02 12:28:52 -0200

    Correção da codificação do arquivo
    Ao converter para anssi, alguns caracteres tiveram problemas. Foi
    realizada a correção.

--- core/glibs/InstallUpgrades.ps1
+++ core/glibs/InstallUpgrades.ps1
@@ -1,4 +1,4 @@
-#ContÈm funÁıes referentes ao processo de instalaÁ„o e upgrade da soluÁıa!
+#Cont√©m fun√ß√µes referentes ao processo de instala√ß√£o e upgrade da solu√ß√£o!
 
 
 #Atualiza de uma versao para outra!
@@ -9,26 +9,26 @@ Function Upgrade {
 	
 	
 	write-host "Upgrading $DestBaseDir from $SourceBaseDir"
-	#ObtÈm todos os sub-diretorios no diretÛrio base
+	#Obt√©m todos os sub-diretorios no diret√≥rio base
 	$Dirs = gci $DestBaseDir | ? {$_.PsIscontainer}
 	
 	
-	#Gera um novo de um diretÛrio de backup.
+	#Gera um novo de um diret√≥rio de backup.
 	#O diretoriio de backup fica no diretorio upgrade.
 	$Backupdir = GetUpgradeBackupDir $DestBaseDir
 	
-	#Faz o backup da estrutura existente para recuperaÁ„o em caso de erros!
+	#Faz o backup da estrutura existente para recupera√ß√£o em caso de erros!
 	try {
-		#Copia os diretÛrios, exceto o diretorio de upgrade, pois ele pode contar muitos outros arquivos!!
+		#Copia os diret√≥rios, exceto o diretorio de upgrade, pois ele pode contar muitos outros arquivos!!
 		$Dirs | ?{ 'upgrade' -NotContains $_.Name} | copy -recurse -Destination $Backupdir;
 	} catch {
 		throw "CANNOT_BACKUP_EXISTENT_DIR: BackupDir: $BackupDir Error: $_";
 	}
 	
-	#Remove toda a estrutura existente, que ser· substituÌda pela nova!
-	write-host "Removendo diretÛrios existentes!"
-	$Removed = @(); #Esta vari·vel ir· conter os diretÛrio que j· foram removidos!
-	$InRollback = $false #Esta vari·vel ir· indicar se o processo entrou em rollback.
+	#Remove toda a estrutura existente, que ser√° substitu√≠da pela nova!
+	write-host "Removendo diret√≥rios existentes!"
+	$Removed = @(); #Esta vari√°vel ir√° conter os diret√≥rio que j√° foram removidos!
+	$InRollback = $false #Esta vari√°vel ir√° indicar se o processo entrou em rollback.
 	$StartRollback = $true;
 	#Para sub-diretorio...
 	$StartRollback = $false;
@@ -37,7 +37,7 @@ Function Upgrade {
 			$CurrentDir = $_;
 			write-host "Current dir is: $($CurrentDir.FullName)"
 			
-			#Se for o diretÛrio config, ignora, pois estes podem conter dados do usu·rio...
+			#Se for o diret√≥rio config, ignora, pois estes podem conter dados do usu√°rio...
 			if( @('config','log','upgrade') -Contains $CurrentDir.Name){
 				return;
 			}
@@ -57,7 +57,7 @@ Function Upgrade {
 		}
 	} catch {
 		if($StartRollback){
-				write-host "Erro ao remover o diretÛrio $($CurrentDir.Name). Iniciando ROLLBACK. Cause: $OriginalError";
+				write-host "Erro ao remover o diret√≥rio $($CurrentDir.Name). Iniciando ROLLBACK. Cause: $OriginalError";
 				UpgradeRollbackRemove -BackupDir $BackupDir -DestDir $DestBaseDir -Removed $Removed -CurrentDir $CurrentDir
 				throw "UPGRADE_FAILED: $OriginalError"
 				return;
@@ -87,7 +87,7 @@ Function Upgrade {
 		
 	} catch {
 		if($StartRollback){
-			write-host "Erro ao copiar o diretÛrio $($CurrentDir.Name). Iniciando ROLLBACK. Cause: $OriginalError";
+			write-host "Erro ao copiar o diret√≥rio $($CurrentDir.Name). Iniciando ROLLBACK. Cause: $OriginalError";
 			UpgradeRollbackCopy -Copiados $Copiados
 			UpgradeRollbackRemove -BackupDir $BackupDir -DestDir $DestBaseDir -Removed $Removed -CurrentDir $null
 			throw "UPGRADE_FAILED: $OriginalError"
@@ -99,7 +99,7 @@ Function Upgrade {
 	write-host "Sucess!"
 }
 
-#Faz o rollback da remoÁ„o dos arquivos...
+#Faz o rollback da remo√ß√£o dos arquivos...
 Function UpgradeRollbackRemove {
 	param($BackupDir, $DestDir, $Removed, $CurrentDir)
 	
@@ -110,11 +110,11 @@ Function UpgradeRollbackRemove {
 		
 		try {
 		
-		#Se existem diretÛrios que j· foram removidos, copia os mesmos inteiros de volta...
+		#Se existem diret√≥rios que j√° foram removidos, copia os mesmos inteiros de volta...
 			if($Removed){
-				write-host "Restabelecendo diretÛrios j· removidos!"
+				write-host "Restabelecendo diret√≥rios j√° removidos!"
 				
-				#Item n„o pode ser removido, fazendo rollback do que j· foi removido!
+				#Item n√£o pode ser removido, fazendo rollback do que j√° foi removido!
 				$Removed | %{
 					write-host "Restaurando backup de $($_.FullName) para $($DestDir)"
 					$BackupPath = $BackupDir  + $_.Name;
@@ -124,35 +124,35 @@ Function UpgradeRollbackRemove {
 			
 			if($CurrentDir){
 				
-				#Determina o caminho para o ˙ltimo diretÛrio, no diretÛrio de backup.
-				write-host "Restaurando itens do ˙ltimo diretÛrio onde ocorreu a falha..."
+				#Determina o caminho para o √∫ltimo diret√≥rio, no diret√≥rio de backup.
+				write-host "Restaurando itens do √∫ltimo diret√≥rio onde ocorreu a falha..."
 				$BackupPath = $BackupDir + $CurrentDir.Name;
 				
 				write-host "Backup path: $BackupPath"
 				#Para cada arquivo que existe no diretorio de backup...
 				gci ($BackupPath+'\*') -recurse | %{
-					$Backup_ArquivoAtual 	= $_; #ContÈm uma referÍncia para o item que representa o arquivo atual...
-					#Indica se o item È um diretÛrio...
+					$Backup_ArquivoAtual 	= $_; #Cont√©m uma refer√™ncia para o item que representa o arquivo atual...
+					#Indica se o item √© um diret√≥rio...
 					$IsDir				= $Backup_ArquivoAtual.PsIscontainer;
 					
-					#ObtÈm o caminho relativo para o arquivo, a partir da raiz da soluÁ„o no backup...
+					#Obt√©m o caminho relativo para o arquivo, a partir da raiz da solu√ß√£o no backup...
 					$Backup_PathRelative	= GetRelativePath -FullPath $Backup_ArquivoAtual.FullName -BaseDir $BackupDir 
 					
 					#Monta o caminho para o arquivo original...
 					$OriginalPath		= $DestDir + $Backup_PathRelative;
 					
 					
-					#Se for um diretÛrio...
+					#Se for um diret√≥rio...
 					if( $IsDir ){
-						#Se o diretorio n„o existir, copia do backup...
+						#Se o diretorio n√£o existir, copia do backup...
 						if(![System.IO.Directory]::Exists($OriginalPath)){
-							write-host "Item n„o existe, copiando de $($Backup_ArquivoAtual.FullName) para $OriginalPath"
+							write-host "Item n√£o existe, copiando de $($Backup_ArquivoAtual.FullName) para $OriginalPath"
 							copy $Backup_ArquivoAtual.FullName $OriginalPath -force -recurse
 						} 
 					} else {
-						#Se o arquivo n„o existir, copia do backup...
+						#Se o arquivo n√£o existir, copia do backup...
 						if(![System.IO.File]::Exists($OriginalPath)){
-							write-host "Item n„o existe, copiando de $($Backup_ArquivoAtual.FullName) para $OriginalPath"
+							write-host "Item n√£o existe, copiando de $($Backup_ArquivoAtual.FullName) para $OriginalPath"
 							#usa o New-Item pra criar o arquivo primeiro antes de copiar o conteudo e evitar o erro...
 							New-Item -force $OriginalPath -Type File | Out-null;
 							copy $Backup_ArquivoAtual.FullName  $OriginalPath -force -recurse
@@ -170,7 +170,7 @@ Function UpgradeRollbackRemove {
 		return;
 }
 
-#Faz o rollback da cÛpia de novos arquivos!
+#Faz o rollback da c√≥pia de novos arquivos!
 Function UpgradeRollbackCopy {
 	param($Copiados)
 	
@@ -183,7 +183,7 @@ Function UpgradeRollbackCopy {
 	
 }
 
-#Instala a soluÁ„o
+#Instala a solu√ß√£o
 Function InstallSolution {
 	param($DestinationBase, $SourceBaseDir)
 	
@@ -191,11 +191,11 @@ Function InstallSolution {
 	
 	write-host "Creating new em $_";
 	
-	#Se o diretorio de install n„o existe, cria-o.
+	#Se o diretorio de install n√£o existe, cria-o.
 	if(![System.IO.Directory]::Exists($DestinationBase)){
 		mkdir $DestinationBase | out-Null
 	} else{
-		write-host "DiretÛrio j· existe... Limpando..."
+		write-host "Diret√≥rio j√° existe... Limpando..."
 		gci ($DestinationBase+'\*') -recurse | Remove-Item -force -recurse;
 	}
 
@@ -210,7 +210,7 @@ Function InstallSolution {
 		} catch {
 			$OriginalError = $_;
 			try {
-				write-host "Erro na instalaÁ„o: $_. O procesos de rollback sera realizado!"
+				write-host "Erro na instala√ß√£o: $_. O procesos de rollback sera realizado!"
 				gci $DestinationBase -recurse | Remove-Item -force -recurse;
 			} catch {
 				throw "FATAL_ERROR_CANNOT_ROLLBACK: $_"
